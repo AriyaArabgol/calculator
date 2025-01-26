fa9799b239f3a839f852d531c83be8f5d37c592d
ariya arabgol <ariya.arabgol@gmail.com>
2025-01-26 12:32:37 +0330

    test

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <calculatorz/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import calculatorz from './components/calculatorz.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    calculatorz
   },
 };
 </script>
